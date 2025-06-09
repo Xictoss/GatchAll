94aafb13ef7a2847916c84e583383c011cce9bd5
Dydou_irl <MegaFight63@laposte.net>
2025-06-10 01:49:57 +0200

    Debut de Menu (ça marche c'est beau !)

--- Assets/Project/Script/Collections/Collection.cs
+++ Assets/Project/Script/Collections/Collection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -10,6 +11,7 @@ namespace GatchAll.Project.Script.Collections
         [field: SerializeField] public KeyCode Key { get; private set; }
         [field: SerializeField] public Transform DebugTransform { get; private set; }
         [field: SerializeField] public GameObject DebugGO { get; private set; }
+        [field: SerializeField] public TextMeshProUGUI Text { get; private set; }
 
         private Item currentUnboxedItem;
 
@@ -17,10 +19,11 @@ namespace GatchAll.Project.Script.Collections
         {
             ItemData itemData = Data.ItemDatas[Random.Range(0, Data.ItemDatas.Count)];
             currentUnboxedItem = itemData.GetItem();
+            Text.text = $"item unlocked: {currentUnboxedItem.Data.Name} Float: {currentUnboxedItem.Float} Description : {currentUnboxedItem.Data.Description}";
         }
 
         
-        //Debug pour spawn les trucs
+        /*Debug pour spawn les trucs
         private void Update()
         {
             if (Input.GetKeyDown(Key))
@@ -36,14 +39,12 @@ namespace GatchAll.Project.Script.Collections
                     Destroy(DebugGO);
                     DebugGO = null;
                 }
-                if (currentUnboxedItem.Data.itemPrefab != null)
+                if (currentUnboxedItem.Data.itemPrefab != null && DebugTransform != null)
                 {
                     DebugGO = Instantiate(currentUnboxedItem.Data.itemPrefab, DebugTransform.position, Quaternion.identity);
                     Debug.Log("Item Prefab found!");
 
                 }
-
-                
                 
                 if (currentUnboxedItem != null)
                 {
@@ -51,6 +52,7 @@ namespace GatchAll.Project.Script.Collections
                               $" a float value of {currentUnboxedItem.Float} and a description of {currentUnboxedItem.Data.Description}" );
                 }
             }
+            Fin debug pour spawn les trucs*/
+
         }
     }
-}
---
+++ Assets/Project/Script/Managers/MenuManager.cs
@@ -0,0 +1,61 @@
+﻿using GatchAll.Project.Script.Collections;
+using LTX.Singletons;
+using TMPro;
+using UnityEngine;
+
+namespace GatchAll.Project.Script.Managers
+{
+    public class MenuManager : MonoSingleton<MenuManager>
+    {
+        [SerializeField] private GameObject CollectionMenu1;
+        [SerializeField] private GameObject CollectionMenu2;
+        [SerializeField] private GameObject Menu;
+        [SerializeField] private TextMeshProUGUI ItemText1;
+        [SerializeField] private TextMeshProUGUI ItemText2;
+        
+        [field: SerializeField] public Collection Collection1 { get; private set; }
+        [field: SerializeField] public Collection Collection2 { get; private set; }
+
+
+        public void OpenCollectionMenu1()
+        {
+            Menu.SetActive(false);
+            CollectionMenu1.SetActive(true);
+        }
+        
+        
+        public void OpenCollectionMenu2()
+        {
+            Menu.SetActive(false);
+
+            CollectionMenu2.SetActive(true);
+        }
+
+        public void CloseCollectionMenu1()
+        {
+            Menu.SetActive(true);
+
+            CollectionMenu1.SetActive(false);
+        }
+        
+        public void CloseCollectionMenu2()
+        {
+            Menu.SetActive(true);
+
+            CollectionMenu2.SetActive(false);
+        }
+
+        public void OpenCaseCollection1()
+        {
+            Collection1.GetUnboxedItemData();
+            ItemText1.text = Collection1.Text.text;
+        }
+        
+        public void OpenCaseCollection2()
+        {
+            Collection2.GetUnboxedItemData();
+            ItemText2.text = Collection2.Text.text;
+        }
+        
+    }
+}
